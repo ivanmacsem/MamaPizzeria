11dac7ffa9e07173d38717d74541fa6b9bed9267
Albert Tang <tangalbert919@yahoo.com>
2022-11-02 13:28:05 -0500

    Pizza.cs: Check for ingredients, redo documentation

--- Assets/Scripts/Pizza.cs
+++ Assets/Scripts/Pizza.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Pizza : MonoBehaviour
 {
-    // 0 - Arugula quantity
-    // 1 - Basil quantity
-    // 2 - Ham quantity
-    // 3 - Chicken quantity
-    // 4 - Pepperoni quantity
-    // 5 - Supreme Mix quantity
-    // 6 - Mushrooms quantity
-
-    //1 = 1/4
-    //2 = 1/2
-    int[] pizza = new int[7];
-    public void AddIngredient(Ingredient i, int quant){
+    /**
+     * This array represents the ingredients on the pizza in the following order:
+     * [Arugula, Basil, Ham, Chicken, Pepperoni, Supreme Mix, Mushrooms]
+     * 0 = No quantity, 1 = 1/4, 2 = 1/2.
+     * Example: 0110200 = Pizza with 1/4 Basil, 1/4 Ham, and 1/2 Pepperoni.
+     */
+    int[] ingredients = new int[7];
+    public void AddIngredient(Ingredient i, int quant)
+    {
         int index = i.GetIngredient();
-        pizza[index] += quant;
+        ingredients[index] += quant;
+    }
+
+    public bool IsPizzaFull()
+    {
+        int sum = 0;
+        for (int i = 0; i < ingredients.Length; i++)
+            sum += ingredients[i];
+        if (sum == 4) // Pizza is full.
+            return true;
+        // Pizza is not full.
+        return false;
+    }
+
+    public int[] GetPizzaIngredients()
+    {
+        return ingredients;
     }
 }
