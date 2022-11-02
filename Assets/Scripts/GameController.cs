ed98d6d1300dae38fa91e554a816421e1fa1df59
Albert Tang <tangalbert919@yahoo.com>
2022-11-02 15:12:35 -0500

    Track score and wrong orders
    The logic for this should probably be moved to its own method.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ using Random = UnityEngine.Random;
 public class GameController : MonoBehaviour
 {
     private Pizza expectedPizza;
+    private int currentScore = 0, wrongOrders = 0;
     [SerializeField] Pizza currentPizza;
     [SerializeField] Text currentScoreView = null;
     void Start()
@@ -36,6 +37,31 @@ public class GameController : MonoBehaviour
         //currentPizza.AddIngredient(1);
         PlayerPrefs.SetInt("curScore", ingredient);
         Console.WriteLine("add ingredient to current pizza");
-        currentScoreView.text = ingredient.ToString();
+        //currentScoreView.text = ingredient.ToString();
+
+        // Maybe move this to separate method?
+        if (currentPizza.IsPizzaFull())
+        {
+            // Compare the two pizzas.
+            int[] temp = currentPizza.GetPizzaIngredients();
+            int[] temp2 = expectedPizza.GetPizzaIngredients();
+            if (temp.Equals(temp2)) // The pizzas are equal.
+            {
+                currentScore++;
+                currentScoreView.text = currentScore.ToString();
+                if (wrongOrders > 0)
+                    wrongOrders = 0;
+            }
+            else // The pizzas are not equal.
+            {
+                currentScore = 0;
+                wrongOrders++;
+                if (wrongOrders == 3)
+                {
+                    // TODO: Implement "You're fired" screen before making this work.
+                }
+                currentScoreView.text = currentScore.ToString();
+            }
+        }
     }
 }
