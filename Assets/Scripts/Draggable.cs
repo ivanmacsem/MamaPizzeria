beb0285e34a850f71cf715ccb9903ba824df7c7c
Mariana Lucio <marianaclucio@outlook.com>
2022-11-11 17:17:07 -0600

    Drag and Drop
    drag and drop files with some prefab stuff.

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,3 +1,5 @@
+
+using System.Net.NetworkInformation;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,37 +7,36 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    //If image doesn't move in proportion to the canvas, add canvas to this slot and divide eventdata.delta by canvas on line 27
-   //[SerializeField] Canvas canvas;
-
     private RectTransform rectTransform;
+    public PizzaSlot currentSlot;
+     public GameObject myPrefab;
+
 
     private void Awake()
     {
-        rectTransform = GetComponent<RectTransform>();
+        //rectTransform = GetComponent<RectTransform>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
 
     {
-        Debug.Log("OnBeginDrag");
+       // Debug.Log("OnBeginDrag");
     }
 
     public void OnDrag(PointerEventData eventData)
 
     {
-        //Debug.Log("OnDrag");
-        //rectTransform.anchoredPosition += eventData.delta;  
-        rectTransform.anchoredPosition += eventData.delta;
+        //rectTransform.anchoredPosition += eventData.delta;
     }
 
     public void OnEndDrag(PointerEventData eventData)
 
     {
-        Debug.Log("OnEndDrag");
+        //Debug.Log("OnEndDrag");
     }
- public void OnPointerDown(PointerEventData eventData)
+    public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
+         Instantiate(myPrefab, new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0), Quaternion.identity);
     }
 }
