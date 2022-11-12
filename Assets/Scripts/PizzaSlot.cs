466ab3911c5fbe651a8489fc7be9034fd8e94e03
Ivan Macsemchuk <ivan.macsemchuk@gmail.com>
2022-11-11 18:51:53 -0600

    Dragging, snapping and destroying

--- Assets/Scripts/PizzaSlot.cs
+++ Assets/Scripts/PizzaSlot.cs
@@ -5,15 +5,13 @@ using UnityEngine.EventSystems;
 
 public class PizzaSlot : MonoBehaviour, IDropHandler
 {
-
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop");
         if (eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<Draggable>().getInstance().GetComponent<RectTransform>().SetParent(GetComponent<RectTransform>());
+            eventData.pointerDrag.GetComponent<Draggable>().getInstance().GetComponent<RectTransform>().anchoredPosition = new Vector2(0,0);
         }
     }
-    
-
 }
